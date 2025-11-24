777ea86eb6a455637449561509ce4253f364c9d1
KamilMurtazin <kamilmurtazin87788@gmail.com>
2025-11-24 16:20:47 +0300

    Delete Homework directory

--- Homework/TextEditor.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class Classwork4_1 {
-	public static void main(String args[]) {
-		Scanner scanner = new Scanner(System.in);
-		String email = scanner.nextLine();
-		email.matches("[a-zA-Z0-9\\._\\+\\-]+@[a-z0-9\\-\\.]+[a-z]{2,}");
